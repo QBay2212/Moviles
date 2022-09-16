8c9c685bcdcb77171705d272502300062ca32a2d
Paulo David <66272982+QBay2212@users.noreply.github.com>
2022-09-16 10:01:08 -0500

    2 activities

---
+++ app/src/main/java/com/example/movilqbay/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.example.movilqbay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+    public void Ingresar(View view){
+        Intent ingresar = new Intent(this, MainActivity.class);
+        startActivity(ingresar);
+
+    }
+
+}
+
+
